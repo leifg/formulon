3517aa1d782e7984a01dfc99f4d878c75f3c318b
Leif Gensert <leif@leif.io>
2016-02-24 23:27:32 +0100

    add tests for add and multiply

--- test/functionLookup.spec.js
+++ test/functionLookup.spec.js
@@ -1,5 +1,17 @@
+var expect = require('chai').expect
+
 let FunctionLookup = require("../src/functionLookup");
 
 describe("FunctionLookup", () => {
-  // TBI
+  describe("#add", () => {
+    it("adds correctly", () => {
+      expect(FunctionLookup.add(1,2)).to.eq(3)
+    })
+  })
+
+  describe("#multiply", () => {
+    it("multiplies correctly", () => {
+      expect(FunctionLookup.multiply(7,8)).to.eq(56)
+    })
+  })
 })
