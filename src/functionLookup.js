bcaaf365d5b0f1e27308913611034c0fae6bab19
Leif Gensert <leif@leif.io>
2016-02-26 00:12:54 +0100

    add more basic arithmetic functions

--- src/functionLookup.js
+++ src/functionLookup.js
@@ -4,9 +4,18 @@ let FunctionLookup = {
   add: function(a, b) {
     return a + b
   },
+  subtract: function(a, b) {
+    return a - b
+  },
   multiply: function(a, b) {
     return a * b
-  }
+  },
+  divide: function(a, b) {
+    return a / b
+  },
+  exponentiate: function(a, b) {
+    return Math.pow(a,b)
+  },
 }
 
 module.exports = FunctionLookup;
--- test/functionLookup.spec.js
+++ test/functionLookup.spec.js
@@ -11,9 +11,27 @@ describe("FunctionLookup", () => {
     })
   })
 
+  describe("#subtract", () => {
+    it("subtracts correctly", () => {
+      expect(FunctionLookup.subtract(10, 100)).to.eq(-90)
+    })
+  })
+
   describe("#multiply", () => {
     it("multiplies correctly", () => {
       expect(FunctionLookup.multiply(7,8)).to.eq(56)
+    })
+  })
+
+  describe("#divide", () => {
+    it("divides correctly", () => {
+      expect(FunctionLookup.divide(10,2)).to.eq(5)
+    })
+  })
+
+  describe("#exponentiate", () => {
+    it("exponentiates correctly", () => {
+      expect(FunctionLookup.exponentiate(2,5)).to.eq(32)
     })
   })
 })
