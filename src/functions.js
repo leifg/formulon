15f781b8e4979ea9b6eb208a85644aa9019574ba
Leif Gensert <leif@leif.io>
2016-03-20 20:56:17 +0100

    name variables like documentation

--- src/functions.js
+++ src/functions.js
@@ -4,42 +4,42 @@ import { buildLiteralFromJs } from "./utils"
 
 // operators
 
-export const sf$add = (a, b) => {
-  return buildLiteralFromJs(a.value + b.value)
+export const sf$add = (value1, value2) => {
+  return buildLiteralFromJs(value1.value + value2.value)
 }
 
-export const sf$subtract = (a, b) => {
-  return buildLiteralFromJs(a.value - b.value)
+export const sf$subtract = (value1, value2) => {
+  return buildLiteralFromJs(value1.value - value2.value)
 }
 
-export const sf$multiply = (a, b) => {
-  return buildLiteralFromJs(a.value * b.value)
+export const sf$multiply = (value1, value2) => {
+  return buildLiteralFromJs(value1.value * value2.value)
 }
 
-export const sf$divide = (a, b) => {
-  return buildLiteralFromJs(a.value / b.value)
+export const sf$divide = (value1, value2) => {
+  return buildLiteralFromJs(value1.value / value2.value)
 }
 
-export const sf$exponentiate = (a, b) => {
-  return buildLiteralFromJs(Math.pow(a.value,b.value))
+export const sf$exponentiate = (value1, value2) => {
+  return buildLiteralFromJs(Math.pow(value1.value,value2.value))
 }
 
 // logical operations
 
-export const sf$and = (a, b) => {
-  return buildLiteralFromJs(a.value && b.value)
+export const sf$and = (logical1, logical2) => {
+  return buildLiteralFromJs(logical1.value && logical2.value)
 }
 
-export const sf$or = (a, b) => {
-  return buildLiteralFromJs(a.value || b.value)
+export const sf$or = (logical1, logical2) => {
+  return buildLiteralFromJs(logical1.value || logical2.value)
 }
 
-export const sf$not = (a) => {
-  return buildLiteralFromJs(!a.value)
+export const sf$not = (logical) => {
+  return buildLiteralFromJs(!logical.value)
 }
 
-// string functions
+// Text Functions
 
-export const sf$trim = (a) => {
-  return buildLiteralFromJs(a.value.trim())
+export const sf$trim = (text) => {
+  return buildLiteralFromJs(text.value.trim())
 }
