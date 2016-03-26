35daad3faaec49c9f23e40f3590e185d1776a48a
Leif Gensert <leif@leif.io>
2016-03-26 10:35:25 +0100

    use correct comparison

--- src/functions.js
+++ src/functions.js
@@ -43,11 +43,11 @@ export const sf$if = (logicalTest, valueIfTrue, valueIfFalse) => {
 }
 
 export const sf$equal = (value1, value2) => {
-  return buildLiteralFromJs(value1.value == value2.value)
+  return buildLiteralFromJs(value1.value === value2.value)
 }
 
 export const sf$unequal = (value1, value2) => {
-  return buildLiteralFromJs(value1.value != value2.value)
+  return buildLiteralFromJs(value1.value !== value2.value)
 }
 
 export const sf$greaterThan = (value1, value2) => {
