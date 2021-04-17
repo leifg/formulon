71a100a87ddd8f6e91662da7afe621dc434dd4bf
Leif Gensert <leif@leif.io>
2021-04-16 17:52:15 -0700

    feat: migrate to peggy

--- .scripts/make-peggy.js
+++ .scripts/make-peggy.js
@@ -1,16 +1,16 @@
-const { generate } = require('pegjs');
+const { generate } = require('peggy');
 const glob = require('glob');
 const { basename } = require('path');
 const { readFileSync, writeFileSync } = require('fs');
 
-glob('grammars/**/*.pegjs', {},(err, files) => {
+glob('grammars/**/*.peggy', {},(err, files) => {
   if(err) {
     console.error(err);
     process.exit(1);
   }
   files.forEach(f => {
     const grammar = readFileSync(f, 'utf8');
-    const outputName = basename(f, '.pegjs');
+    const outputName = basename(f, '.peggy');
     writeFileSync(
       `src/${outputName}.grammar.js`,
       `export default ${generate(grammar, Object.assign({ output: 'source' }, {}))};`
